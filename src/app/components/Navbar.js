65ee861d24e4e5a49f383969f98ce86af6f76c76
kalyan-patel <109415708+kalyan-patel@users.noreply.github.com>
2025-01-16 22:23:09 -0500

    navbar tweaks

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -3,22 +3,22 @@
 import { useAuth } from "../contexts/AuthContext";
 import { useRouter } from "next/navigation";
 import { useState, useEffect, useRef } from "react";
-import { FiInbox, FiPlus } from "react-icons/fi";
+import { FiInbox, FiPlusSquare, FiUser } from "react-icons/fi";
 
 export default function Navbar() {
   const { currentUser, logout } = useAuth();
   const router = useRouter();
   const [dropdownOpen, setDropdownOpen] = useState(false);
-  const [isMobile, setIsMobile] = useState(window.matchMedia('(max-width: 767px)').matches);
+  const [isMobile, setIsMobile] = useState(window.matchMedia("(max-width: 767px)").matches);
   const dropdownRef = useRef(null);
 
   useEffect(() => {
     const handleResize = () => {
-      setIsMobile(window.matchMedia('(max-width: 767px)').matches);
+      setIsMobile(window.matchMedia("(max-width: 767px)").matches);
     };
 
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
   }, []);
 
   useEffect(() => {
@@ -46,13 +46,15 @@ export default function Navbar() {
       {/* Logo */}
       <button
         onClick={() => router.push("/")}
-        className={`text-3xl font-bold text-blue-500 focus:outline-none ${isMobile ? "ml-2" : "md:ml-10 lg:ml-20"}`}
+        className={`text-3xl font-bold text-blue-500 focus:outline-none ${
+          isMobile ? "ml-4" : "md:ml-10 lg:ml-20"
+        }`}
       >
         {(isMobile ? "" : "Tufts ") + "Handoff"}
       </button>
 
       {/* Action Buttons */}
-      <div className="flex items-center space-x-2 md:space-x-4 md:mr-10">
+      <div className="flex items-center space-x-3 md:space-x-4 md:mr-10">
         {/* Messages Button */}
         <button
           onClick={() => router.push("/chat")}
@@ -68,7 +70,7 @@ export default function Navbar() {
           className="p-2 bg-blue-400 rounded-full hover:bg-blue-500 text-white focus:outline-none"
           aria-label="Create New Listing"
         >
-          <FiPlus className="w-6 h-6" />
+          <FiPlusSquare className="w-6 h-6" />
         </button>
 
         {/* Profile Section */}
@@ -79,20 +81,7 @@ export default function Navbar() {
               className="px-4 py-2 bg-gray-300 rounded-lg hover:bg-gray-400 focus:outline-none flex items-center"
             >
               <span className="mr-2 hidden md:block">{currentUser.displayName || "Anonymous"}</span>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className={`h-4 w-4 transition-transform ${
-                  dropdownOpen ? "rotate-180" : "rotate-0"
-                }`}
-                viewBox="0 0 20 20"
-                fill="currentColor"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M5.23 7.21a.75.75 0 011.06-.02L10 10.586l3.71-3.4a.75.75 0 111.02 1.1l-4 3.75a.75.75 0 01-1.04 0l-4-3.75a.75.75 0 01-.02-1.06z"
-                  clipRule="evenodd"
-                />
-              </svg>
+              <FiUser className="w-5 h-5" />
             </button>
             {dropdownOpen && (
               <div className="absolute right-0 mt-2 w-48 bg-white shadow-lg rounded-lg overflow-hidden">
