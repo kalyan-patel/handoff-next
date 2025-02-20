e61da064f29720ecc3abad9b98889bcc6e4353d2
kalyan-patel <109415708+kalyan-patel@users.noreply.github.com>
2025-02-19 23:34:36 -0500

    margin attempt fix but not complete

--- src/app/components/Listings.js
+++ src/app/components/Listings.js
@@ -39,7 +39,7 @@ const Listings = () => {
 
   return (
     <div className="mx-auto px-4 md:px-12 py-10">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {listings.map((listing) => (
           <div
             key={listing._id}
